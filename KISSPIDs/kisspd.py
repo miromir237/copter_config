aad376b4dd59931e75a02100c13cb96abe297c6b
miromir237 <miroslav.pilat@gmail.com>
2021-03-07 13:29:00 +0100

    KISSPID calculator inital

---
+++ KISSPIDs/kisspd.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# KISS P/D ratio calculator
+# Input P value for  Pitch axis
+# Output suggested P,D values for Pitch and Roll axis
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-p','--pitchp', help="PitchP", required=True, type=float, dest='p')
+parser.add_argument('-rp','--rpdiff', help="PitchP", required=False, type=float, default=0.15, dest='rp')
+
+args = parser.parse_args()
+pitchp = float(args.p)
+rpdiff = float(args.rp)
+#pitchp = input("Input P value for Pitch axis: ")
+
+pitchd_tmp = ((10*pitchp) / 2)
+pitchd = pitchd_tmp - (pitchd_tmp*0.3)
+
+rollp = pitchp - (pitchp * rpdiff)
+rolld = pitchd - (pitchd * rpdiff)
+
+print("Starting PIDs based on Pitch P value of " + str(pitchp) +"\n")
+print("Pitch: P(" + str(pitchp) + ") D(" + str(pitchd) + ")\n")
+print("Roll : P(" + str(rollp) + ") D(" + str(rolld) + ")\n")
