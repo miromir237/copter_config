0c9e00fd0e942df1c61bc4f40d3dd84042f0cfc4
miromir237 <miroslav.pilat@gmail.com>
2021-03-07 13:40:55 +0100

    Adding KISSPD calc

--- KISSPIDs/kisspd.py
+++ KISSPIDs/kisspd.py
@@ -7,13 +7,12 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-p','--pitchp', help="PitchP", required=True, type=float, dest='p')
-parser.add_argument('-rp','--rpdiff', help="PitchP", required=False, type=float, default=0.15, dest='rp')
+parser.add_argument('-p','--pitchp', help="Pitch P value", required=True, type=float, dest='p')
+parser.add_argument('-rp','--rpdiff', help="R/P percentage difference", required=False, type=float, default=0.15, dest='rp')
 
 args = parser.parse_args()
 pitchp = float(args.p)
 rpdiff = float(args.rp)
-#pitchp = input("Input P value for Pitch axis: ")
 
 pitchd_tmp = ((10*pitchp) / 2)
 pitchd = pitchd_tmp - (pitchd_tmp*0.3)
@@ -22,5 +21,6 @@ rollp = pitchp - (pitchp * rpdiff)
 rolld = pitchd - (pitchd * rpdiff)
 
 print("Starting PIDs based on Pitch P value of " + str(pitchp) +"\n")
-print("Pitch: P(" + str(pitchp) + ") D(" + str(pitchd) + ")\n")
 print("Roll : P(" + str(rollp) + ") D(" + str(rolld) + ")\n")
+print("Pitch: P(" + str(pitchp) + ") D(" + str(pitchd) + ")\n")
+
